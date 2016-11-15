d15e45f1a695b2d5b5430297c4280815623899ab
omacronpercyieight27 <ahmadnazir847@gmail.com>
2016-11-15 23:54:36 +0000

    11/15/16

---
+++ config/routes.rb
@@ -0,0 +1,38 @@
+Rails.application.routes.draw do
+ 
+  resources :pins
+  devise_for :users
+  root 'pins#index'
+  get 'home/about'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
